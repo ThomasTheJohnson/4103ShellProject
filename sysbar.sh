cc85b084c7f20c5c90ade20b808fb7f163dde8ff
Thomas Johnson <tjohnson061944@gmail.com>
2017-04-25 11:11:27 -0500

    I got it working! Woohoo.

--- sysbar.sh
+++ sysbar.sh
@@ -1,5 +1,49 @@
 #!/bin/bash
 
+cpuPlot(){
+
+cat > cpu_usage.plot << EOF
+=stacked;  usr; sys; idle
+max=100
+legendx=right
+legendy=center
+title=CPU Utilization
+colors=magenta,yellow,dark_blue
+xlabel=Time
+ylabel=Percentage (%)
+=table
+=norotate
+yformat=%g%%		
+
+EOF
+}
+
+ioPlot(){
+
+cat > disk_usage.plot << EOF
+=stacked; kB_read; kB_write
+legendx=right
+legendy=center
+title=Disk Utilization
+colors=yellow,dark_blue
+xlabel=Time
+ylabel=Bandwidth (kB/sec)
+=table
+=norotate
+yformat=%g
+
+EOF
+}
+
+writeCpu(){
+paste <(date +%H:%M:%S) <(mpstat | awk 'NR==4{print $4 "\t" $6 "\t" $13}') >> cpu_usage.plot ;
+}
+
+writeIo(){
+paste <(date +%H:%M:%S) <(iostat | awk 'NR==7{print $3 "\t" $4}') >> disk_usage.plot ;
+}
+
+
 case $1 in
 	#Case 1.1
 	help)
@@ -7,13 +51,28 @@ case $1 in
 		;;#End of Case 1.1 
 	
 	#Case 1.2
-	[1-9]|[1-5][1-9]|[6][0]) 
+	[1-9]|[1-5][0-9]|[6][0]) 
 		
 		case $2 in
-			#Case 1.21
+			#Case 1.21 "Main Execution"
 			[1-9]|[1][0]) 
 				echo "$1 and $2 were the inputs"
+				
+				cpuPlot
+				ioPlot
+				
+				for (( i=0; i<$2; i++ )); do
+					writeCpu
+					writeIo
+					sleep $1
+				done
+
+				perl bargraph.pl cpu_usage.plot > cpu_usage.eps
+				perl bargraph.pl disk_usage.plot > disk_usage.eps
+				rm cpu_usage.plot
+				rm disk_usage.plot					
 				;;#End of Case 1.21
+
 			#Case 1.22
 			*)
 				echo "Incorrect usage. Second argument invalid For correct usage information use the command:\n ./sysbar help"
